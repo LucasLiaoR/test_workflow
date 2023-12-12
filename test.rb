2c13ca41bf66f4b861a26216a0e2b0cc1ee862f1
Lucas Liao <lucasliao6@gmail.com>
2023-12-12 15:17:40 +0100

    toto

--- test.rb
+++ test.rb
@@ -1,3 +1,8 @@
 ARGV.each do |start_date, end_date, selected_accounts_ids|
-  puts "#{start_date}, #{end_date}, #{selected_accounts_ids}"
+  ids = selected_accounts_ids.split(',')
+
+  puts "#{start_date}, #{end_date}"
+  puts "first id: #{ids.first}"
+  puts "second id: #{ids[1]}"
+  puts "last id: #{ids.last}"
 end
